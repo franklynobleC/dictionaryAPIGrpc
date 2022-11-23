acba7e29173b2390f03d7befeae64034dd0f6474
franklynobleC <franklyessien01@gmail.com>
2022-11-23 04:03:02 -0500

    correct pulsar import error

--- pulsarClient/consumer/consumer.go
+++ pulsarClient/consumer/consumer.go
@@ -1,11 +1,41 @@
 package main
 
 import (
-	"github.com/Comcast/pulsar-client-go"
-	 "github.com/apache/pulsar-client-go/pulsar"
-)
-func main() {
-	
+	"context"
+	"errors"
+     "log"
 
-	
+	"github.com/apache/pulsar-client-go/pulsar"
+	// "github.com/Comcast/pulsar-client-go"
+	"github.com/apache/pulsar/pulsar-function-go/pf"
+)
+
+func PublishFunc(ctx context.Context, in []byte) error {
+	fctx, ok := pf.FromContext(ctx)
+
+	if !ok {
+		return errors.New("get Go functions Context error")
+	}
+
+
+	publishTopic := "publish-topic"
+	output := append(in, 110)
+
+	producer := fctx.NewOutputMessage(publishTopic)
+	msgID, err := producer.Send(ctx, &pulsar.ProducerMessage{
+		Payload: output,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("The output message ID is: %+v", msgID)
+	return nil
+}
+
+
+func main() {
+  cleint, err := pulsar.NewClient(pulsar.ClientOptions{}
+)
+
 }
